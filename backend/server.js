1e206232b82333478dd2b651bd18c304e231b50e
krishnavamsigara <garakrishnavamsi2004@gmail.com>
2025-07-01 15:22:51 +0530

    codechanging

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 import dotenv from 'dotenv';
-import { createServer } from 'http';
+import http from 'http'
+
 import { Server } from 'socket.io';
 
 dotenv.config();
 
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: '*' },
 });
@@ -112,6 +113,12 @@ app.post('/run', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+
+if(process.env.NODE_ENV !== "production"){
+  server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+}
+
+export default server
+
